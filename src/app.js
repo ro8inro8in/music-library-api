e99e04ef1174ccbeaded76f276336c750c4f46a7
Robin Edwards <robinedwards1987@gmail.com>
2020-12-29 16:37:17 +0000

    Artist halfway

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
+//const port = 4000;
 app.use(express.json())
+//app.use(express.urlencoded())
 
-
-
+app.get('/', (req, res) => {
+    res.send('I know what you did last summer!!!')
+  })
+  
 
 
 
---
+++ src/models/index.js
@@ -0,0 +1,23 @@
+const Sequelize = require('sequelize');
+const ArtistModel = require('./artist');
+
+const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
+//--V--Function that is connecting to my .env file--V--//
+const setupDatabase = () => {
+    const connection = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+        host: DB_HOST,
+        port: DB_PORT,
+        dialect: 'mysql',
+        logging: false,
+
+        });
+
+        const Artist = ArtistModel(connection, Sequelize);
+
+        connection.sync({ alter: true });
+        return {
+            Artist
+        };
+    };
+
+module.exports = setupDatabase();
