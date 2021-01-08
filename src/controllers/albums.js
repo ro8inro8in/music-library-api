a3901411216930c5572564779a185e0e80b7459f
Robin Edwards <robinedwards1987@gmail.com>
2021-01-08 15:56:25 +0000

    Broken Albums for Ian to look at

--- src/controllers/albums.js
+++ src/controllers/albums.js
@@ -1,20 +1,24 @@
-const { Album, Artist } = require("../models");
+const { Album, Artists } = require("../models");
 
 exports.create = (req, res) => {
-  const { artistId } = req.params;
+  const data = req.body;
+  data.ArtistId = req.params.id;
+  //console.log(album);
+  Album.create(req.body).then((album) => res.status(201).json(album));
 };
-Artist.findByPk(artistId).then((artist) => {
-  if (!artist) {
-    res.status(404).json({ error: "The artist could not be found." });
-  } else {
-    Album.create({
-      name: req.body.name,
-      year: req.body.year,
-    }).then((album) => {
-      album.setArtist(artist).then((album) => {
-        res.status(201).json(album);
-      });
-    });
-  }
-});
+exports.list = (req, res) => {
+  Artists.findAll().then((artists) => res.status(200).json(artists));
+};
+
+exports.albumCreate = (req, res) => {
+  console.log(req.params.id)
+  Album.findByPk(req.params.id)
+   .then((res) => {
+          expect(res.status).to.equal(404);
+          expect(res.body.error).to.equal('The artist could not be found.');
+          Album.findAll().then((albums) => {
+            expect(albums.length).to.equal(0);
+            done();
+          });
+        }).catch(error => done(error));
 };
